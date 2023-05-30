de79c4cb73867118a407bcbf6b52ff32a1758167
Rodrigo C. Arruda <rodrigo.arruda@aluno.cefet-rj.br>
2023-05-30 13:23:32 -0300

    Att

--- aula09/conexao.php
+++ aula09/conexao.php
@@ -2,7 +2,7 @@
 
 function GET_CONNECTION()
 {
-  return new PDO('mysql:host=localhost:3308;dbname=aula09;charset=utf8','root','',[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+  return new PDO('mysql:host=localhost:3306;dbname=aula09;charset=utf8','root','',[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 }
 
 ?>
